acce4953d94633dace21f5e6ddb3412e48aafa60
Nate Bolam <nbolam@capturealpha.com>
2025-02-25 21:43:36 -0500

    Fixed possible issue with interrupt function signal.Notify

--- git-remote-helper/helpers.go
+++ git-remote-helper/helpers.go
@@ -25,7 +25,7 @@ func fetchFullBareRepo(root string) (string, error) {
 }
 
 func interrupt() error {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	return errors.Errorf("%s", <-c)
 }
